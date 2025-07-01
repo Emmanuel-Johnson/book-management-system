07e31c781b40b402c77605a3764bd7b9b9fdf614
Emmanuel Johnson <emmanuel.johnson.pro@gmail.com>
2025-07-02 00:32:15 +0530

    ADD Edit, Delete

--- server/newproject/api/urls.py
+++ server/newproject/api/urls.py
@@ -4,5 +4,5 @@ from .views import get_books, create_book, book_detail
 urlpatterns = [
     path('books/', get_books, name='get_books'),
     path('books/create/', create_book, name='create_book'),
-    path('books/edit/pk/', book_detail, name='book_detail')
+    path('books/<int:pk>', book_detail, name='book_detail')
 ]
